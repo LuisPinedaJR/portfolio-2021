95dbc3bd18475d90e2d6fc95927423ca0c5d51c3
Luis Pineda <lspnd00@gmail.com>
2021-05-17 17:42:12 -0400

    portfolio finish 2021

---
+++ src/yourdata.js
@@ -0,0 +1,190 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import node from "./images/node-js.svg"
+import postgres from "./images/postgresql.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import linkedin from "./images/linkedin.svg"
+import medium from "./images/medium.svg"
+import resume from "./images/resume.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Luis",
+  headerTagline: [
+    //Line 1 For Header
+    "I am a",
+    //Line 2 For Header
+    "Front end dev",
+    //Line 3 For Header
+    "and tech student :)",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "My tech stack includes ReactJS, CSS, NodeJS, Java, HTML, check back later to see what else I have added to toolkit, I love learning new things and always looking to better myself. ",
+
+  //Contact Email
+  contactEmail: "Luispineda@hey.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Github Battle", //Project Title - Add Your Project Title Here
+      para: "Battle against github users for best stats.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: "https://i.postimg.cc/dtPz81pJ/githubbattle.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github-battle-6b178.firebaseapp.com/",
+    },
+    {
+      title: "Indecision App", //Project Title - Add Your Project Title Here
+      para:
+        "This is built to help you make up your mind on what todo, eat or maybe say?", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.postimg.cc/yNkQq4tf/Screen-Shot-2021-05-17-at-4-17-54-PM.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://infallible-mcclintock-92b215.netlify.app/",
+    },
+    {
+      title: "Notes App", //Project Title - Add Your Project Title Here
+      para:
+        "a Simple todo app that auto saves for you, no need to hit save and also lets you add comments to your notes :)", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.postimg.cc/90rFZfnd/Screen-Shot-2021-05-17-at-4-21-20-PM.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://notes-app-a047ca.netlify.app/index.html",
+    },
+    {
+      title: "Ride for Life", //Project Title - Add Your Project Title Here
+      para:
+        "Safe & efficient transportation to healthcare facility for mothers in labor.Serving mothers in Uganda to improve maternal & child survival", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: "https://i.postimg.cc/vHKXWScf/Head.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://eager-shirley-4c7338.netlify.app/",
+    },
+    {
+      title: "African Marketplace", //Project Title - Add Your Project Title Here
+      para:
+        "Empowers small business owners, particularly women in Africa, to improve their business and economic opportunities to grow out of poverty. The African Marketplace is a platform that allows sellers in Sauti Africa to reach a larger demographic to sell their goods.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: "https://i.postimg.cc/tCCnXhtc/africanmarket.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://african-market-place2.netlify.app/",
+    },
+    {
+      title: "Express Groomer", //Project Title - Add Your Project Title Here
+      para:
+        "Express Groomer connects groomers and clients. The connection allows for groomers to meet at the clients residence or to meet at the groomers residence. ", // Add Your Service Type Here
+      //Project Image - Add Your Project 4Image Here
+      imageSrc:
+        "https://i.postimg.cc/yYpGnC18/Screen-Shot-2021-02-03-at-11-25-46-PM.png",
+      //Project URL - Add Your Project Url Here
+      url:
+        "https://github.com/Lambda-School-Labs/deprecated-Express_Groomer-TeamA-FE",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "MY journey started during MySpace years, I loved adding new pieces of code to my page and liked seeing the changes on the browser, however, I didn't know I could do that as a career.",
+  aboutParaTwo:
+    "I have been studying web development on my own for about 3 years and completed task work such as Udemy courses, Code academy, etc. I joined Lambda School and my knowledge of computer science and software engineering accelerated and gave me the confidence to work for myself. ",
+  aboutParaThree:
+    "Today, I currently am a Freelance ReachJS contractor always looking for challenging roles and projects. I am continuing my studies of tech, there is always something new to learn, currently studying design and data science.",
+  aboutParaFour:
+    "Outside of coding, I like to use my hands to get crafty and paint, I am not picasso but it helps relieve stress and it is relaxing. I am a father to a one year old and I truly love being a father, including the changing diapers part. I also enjoy traveling and taking pictures of different architecture structures.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1621278517080-26c20563f7ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para: "HTML5",
+    },
+    {
+      img: cssIcon,
+      para: "CSS3",
+    },
+    {
+      img: jsIcon,
+      para: "JavaScript",
+    },
+    {
+      img: reactIcon,
+      para: "React.js",
+    },
+    {
+      img: node,
+      para: "Node.js",
+    },
+    {
+      img: postgres,
+      para: "PostgresSQL",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "The stack",
+  promotionPara:
+    "The primary technologies I work with are React.js, JavaScript, Node.js, Express, PostgreSQL, HTML and CSS for building projects. I've created solo full-stack applications, participated in team hackathons, and collaborated with teammates to bring project ideas to life.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/LuisPinedaJR" },
+
+    {
+      img: linkedin,
+      url: "https://www.linkedin.com/in/luisfpineda1/",
+    },
+    {
+      img: medium,
+      url: "https://medium.com/@luispinedadev",
+    },
+    {
+      img: resume,
+      url:
+        "https://docs.google.com/document/d/1jXy-agSSX9hmdSegocAqaTnzI1__RB6qRWFY8YJM_7A/edit?usp=sharing",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
